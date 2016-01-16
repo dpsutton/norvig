9b7f2b17a9d67b58c6ab27e25854dc212f548b46
Dan Sutton <danielsutton01@gmail.com>
2016-01-16 14:47:14 -0600

    generate parse tree with sentence

--- sentence-grammar.lisp
+++ sentence-grammar.lisp
@@ -36,6 +36,16 @@
 
 (setf *grammar* *bigger-grammar*)
 
+(defun generate-tree (phrase)
+  "Generate a random sentence or phrase, with a complete parse tree."
+  (cond
+    ((listp phrase)
+     (mapcar #'generate-tree phrase))
+    ((rewrites phrase)
+     (cons phrase
+           (generate-tree (random-elt (rewrites phrase)))))
+    (t (list phrase))))
+
 (defun generate (phrase)
   "Generate a random sentence or phrase."
   (cond
