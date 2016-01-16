2cbcbce304c323f85caf22f6307f9050d520efa4
Dan Sutton <danielsutton01@gmail.com>
2016-01-16 17:54:00 -0600

    generate all combinations
    careful not to use on an infinite grammar

--- sentence-grammar.lisp
+++ sentence-grammar.lisp
@@ -46,6 +46,25 @@
            (generate-tree (random-elt (rewrites phrase)))))
     (t (list phrase))))
 
+(defun generate-all (phrase)
+  "Generate a list of all possible expansions of this phrase."
+  (cond
+    ((null phrase) (list nil))
+    ((listp phrase)
+     (combine-all (generate-all (first phrase))
+                  (generate-all (rest phrase))))
+    ((rewrites phrase)
+     (mappend #'generate-all (rewrites phrase)))
+    (t (list (list phrase)))))
+
+(defun combine-all (xlist ylist)
+  "Return a list of lists formed by appending a y to an x.
+E.g., (combine-all '((a) (b)) '((1) (2)))
+-> ((A 1) (B 1) (A 2) (B 2))"
+  (mappend #'(lambda (y)
+               (mapcar #'(lambda (x) (append x y)) xlist))
+           ylist))
+
 (defun generate (phrase)
   "Generate a random sentence or phrase."
   (cond
